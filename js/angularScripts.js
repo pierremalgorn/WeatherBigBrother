94a6a77af30bb529791b571d94ce67b4a361dbba
Pierre <pierre.malgorn@sfr.fr>
2015-03-26 19:59:36 +0100

    Modal display

--- js/angularScripts.js
+++ js/angularScripts.js
@@ -1,11 +1,18 @@
 var WeatherBigBrother = angular.module('WeatherBigBrother', []).controller('MapController', function($scope, $http){
+    
+    $scope.points = [
+        {city:'Paris,fr', top:33, left:46},
+        {city:'New York,us', top:37, left:27},
+      ];
+    
     $scope.cityWeather = [];
     
     $scope.getWeatherForCity = function(city){
+        console.log(city);
         $http.get('http://api.openweathermap.org/data/2.5/weather?q='+city)
             .success(function(data){
                  $scope.cityWeather = data;
-         console.log(data);
+                 $('#modalWeatherForCity').modal('show');
         })
             .error(function(data){
                 //GERER L'ERREUR ???
