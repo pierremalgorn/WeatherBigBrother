c9d8c6100ed2ed6949eba1c9da88215fe6050858
Pierre <pierre.malgorn@sfr.fr>
2015-03-26 17:15:58 +0100

    Mises à joursssss

---
+++ js/angularScripts.js
@@ -0,0 +1,16 @@
+var WeatherBigBrother = angular.module('WeatherBigBrother', []).controller('MapController', function($scope, $http){
+    $scope.cityWeather = [];
+    
+    $scope.getWeatherForCity = function(city){
+        $http.get('http://api.openweathermap.org/data/2.5/weather', {q: 'paris,fr'})
+            .success(function(data){
+                 $scope.cityWeather = data;
+         console.log(data);
+        })
+            .error(function(data){
+                //GERER L'ERREUR ???
+                 $scope.cityWeather = data;
+        })
+    };
+
+});
