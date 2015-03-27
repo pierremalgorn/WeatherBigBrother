bfe4a3c629e9776cf88968e676c70e078979627b
Pierre <pierre.malgorn@sfr.fr>
2015-03-27 10:25:52 +0100

    Ajout de coordonnées supplémentaires pour les points

--- js/angularScripts.js
+++ js/angularScripts.js
@@ -19,5 +19,50 @@ var WeatherBigBrother = angular.module('WeatherBigBrother', []).controller('MapC
                  $scope.cityWeather = data;
         })
     };
-
+    
+    $scope.toCelsius = function(temp) {
+        return ((temp - 32) * 5.0/9.0);
+    };
+    
+    $scope.toKMH = function(speed) {
+        return (speed * 3,6);
+    }
+    
+    $scope.coordPoint = function(point, pos) {
+        if(pos == 'top') {
+            if(typeof point.top !== 'undefined') {
+                return (point.top+'%');
+            }
+            else {
+                return 'auto';
+            }
+        }
+        
+        if(pos == 'bottom') {
+            if(typeof point.bottom !== 'undefined') {
+                return (point.bottom+'%');
+            }
+            else {
+                return 'auto';
+            }
+        }
+        
+        if(pos == 'right') {
+            if(typeof point.right !== 'undefined') {
+                return (point.right+'%');
+            }
+            else {
+                return 'auto';
+            }
+        }
+        
+        if(pos == 'left') {
+            if(typeof point.left !== 'undefined') {
+                return (point.left+'%');
+            }
+            else {
+                return 'auto';
+            }
+        }
+    };
 });
