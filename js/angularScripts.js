1052098d8ab3beb096d5dca1022c7a69c75f6627
bitogno <rabassealexandre@gmail.com>
2015-03-31 17:57:22 +0200

    slide de la div

--- js/angularScripts.js
+++ js/angularScripts.js
@@ -5,26 +5,31 @@ var WeatherBigBrother = angular.module('WeatherBigBrother', []).controller('MapC
         {city:'Madrid,es', top:37.1, left:44.14},
         {city:'London_uk', top:30.35, left:45},
         {city:'Berlin_de', top:31.65, left:47.47},
-        {city:'Moscow_ru', top:30.3, left:56.6},
-        {city:'Saint_Petersburg', top:26.36, left:54.14},
-        {city:'New York,us', top:38.4, left:27.4},
-        {city:'Washington,us', top:42.3, left:25.8},
-        {city:'Chicago,us', top:37.1, left:23.3},
-        {city:'Los_Angeles,us', top:43.7, left:14},
-        {city:'Vancoucer,ca', top:34.42, left:12.41},
-        {city:'Mexico,me', top:50.4, left:19.1},
-        {city:'Caracas,ve', top:57.1, left:27.4},
-        {city:'Bogota,co', top:61.1, left:25.8},
-        {city:'Lima,pe', top:66.5, left:24.9},
-        {city:'Sao_Paulo,bra', top:71.87, left:34.12},
-        {city:'Santiago,chile', top:77.21, left:26.6},
-        {city:'Buenos_Aires,ar', top:50, left:50},
-        {city:'Paris,fr', top:33, left:46},
-        {city:'Pélissanne,fr', top:34, left:46},
-        {city:'New York,us', top:37, left:27},
       ];
     
     $scope.cityWeather = [];
+
+    jQuery(document).ready(function($) {
+        var query = window.location.href;
+        var vars = query.split("#");
+        var flag = 0;
+        (vars[1] >= 0 && vars[1] <= $(".moreInfo").size()) ? goTo(vars[1]) : goTo(0);
+        $( ".moreInfo" ).click(function() { 
+            if(flag == 1) {return false;}
+            $(".moreInfo").removeClass("active");
+            $(this).addClass("active")
+            flag = 1;
+            $( ".slide" ).animate({right:'+=100%'}, "slow", function(){
+                flag = 0;
+            });
+        });
+    function goTo(nb){
+            $( ".slide" ).css( "margin-left", - (nb * $("#setup").width()) +"px");
+            $(".moreInfo").removeClass("active");
+            $('.moreInfo:nth('+nb+')').addClass("active");
+            
+        }
+    });
     
     var displayWeather = function() {
         var deg = $scope.cityWeather.wind.deg;
